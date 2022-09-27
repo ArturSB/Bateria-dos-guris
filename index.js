b0070e2f454b1d78ed0aa4b0fd3a005571936a07
Artur Balestra <artursarzi@gmail.com>
2022-09-27 20:09:01 -0300

    efeito do botao adicionado

--- index.js
+++ index.js
@@ -1,36 +1,74 @@
-let numBtn = document.querySelectorAll(".btn");
+let numBtn = document.querySelectorAll(".btn").length;
 
-for (let i = 0; i < numBtn.length; i++) {
-  document.querySelectorAll(".btn")[i].addEventListener("click", function () {
-    switch (i) {
-      case 0:
-        let crash = new Audio("/sounds/crash.mp3");
-        crash.play();
-        break;
-      case 1:
-        let kick = new Audio("/sounds/kick-bass.wav");
-        kick.play();
-        break;
-      case 2:
-        let snare = new Audio("/sounds/snare.mp3");
-        snare.play();
-        break;
-      case 3:
-        let tom1 = new Audio("/sounds/tom-1.mp3");
-        tom1.play();
-        break;
-      case 4:
-        let tom2 = new Audio("/sounds/tom-2.mp3");
-        tom2.play();
-        break;
-      case 5:
-        let tom3 = new Audio("/sounds/tom-3.mp3");
-        tom3.play();
-        break;
-      case 6:
-        let tom4 = new Audio("/sounds/tom-4.mp3");
-        tom4.play();
-        break;
-    }
-  });
+
+// Evento de click do mouse
+
+for (let i = 0; i < numBtn; i++) {
+
+  //adicionando eventListener em todos os botoes com a classe .btn
+
+  document.querySelectorAll(".btn")[i].addEventListener("click", function(){
+
+    let btnText = this.innerText;
+    drumPlay(btnText);
+
+    buttonEffect(btnText);
+
+  })
+}
+
+document.addEventListener("keydown", function(event){
+  let tecla = event.key.toUpperCase();
+  drumPlay(tecla);
+
+  buttonEffect(tecla);
+
+})
+
+//switch para selecionar qual som toca em cada letra clicada pelo mouse
+
+function drumPlay(btnText){
+
+  switch (btnText) {
+    case "A":
+      let crash = new Audio("/sounds/crash.mp3");
+      crash.play();
+      break;
+    case "S":
+      let kick = new Audio("/sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case "D":
+      let snare = new Audio("/sounds/snare.mp3");
+      snare.play();
+      break;
+    case "H":
+      let tom1 = new Audio("/sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "J":
+      let tom2 = new Audio("/sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "K":
+      let tom3 = new Audio("/sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "L":
+      let tom4 = new Audio("/sounds/tom-4.mp3");
+      tom4.play();
+      break;
+  }
+}
+
+function buttonEffect (btn){
+
+  let btnClass = document.querySelector("."+btn.toLowerCase());
+  
+  btnClass.classList.add('btnPressed')
+
+  setTimeout(function(){
+    btnClass.classList.remove('btnPressed')
+  }, 125)
+
 }
